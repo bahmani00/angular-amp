b3f1f81471c18071e7175f59cc0fc693aa75c7f9
bahmani00 <bahmani00@gmail.com>
2021-02-06 19:41:00 -0500

    added ProductList component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,12 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'pm-root',
-  templateUrl: './app.component.html',
+  template: `
+    <div class='container'>
+      <h1><a class='navbar-brand'>{{title}}</a></h1>
+      <pm-products></pm-products>
+    </div>
+    `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
